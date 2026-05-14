a98ad2969a26ce1936dcc54eb848b97b92a362ff
Yufei Cai <248735861+caiyufei8@users.noreply.github.com>
2026-05-14 16:06:27 -0400

    Day 3.

--- py-KernSmooth/tests/test.py
+++ py-KernSmooth/tests/test.py
@@ -1,8 +1,11 @@
+import numpy as np
 import KernSmooth
 
 
 def main():
-    print(dir(KernSmooth))
+    x = np.array([1, 2, 3, 4, 5])
+    y = KernSmooth.bkde(x)
+    print(y)
 
 
 if __name__ == "__main__":
