f74d984b9e5a1fb82b2b888c910ec3597f1db7b3
Yufei Cai <248735861+caiyufei8@users.noreply.github.com>
2026-05-21 16:30:13 -0400

    Squashed 'r2py_kernsmooth/' changes from ee97555..73f0b77
    73f0b77 Day 7.
    
    git-subtree-dir: r2py_kernsmooth
    git-subtree-split: 73f0b77a56d993fd28108715e84bc5881cfbc48c

--- git_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -pe smp 1
-#$ -q long
-module load git
-export PATH=~/bin:$PATH
-# git remove add python-KernSmooth https://github.com/caiyufei8/python-KernSmooth.git
-git push origin main
-# git remote add r2py_kernsmooth https://github.com/caiyufei8/r2py_kernsmooth.git
-git subtree push --prefix=r2py_kernsmooth r2py_kernsmooth main
